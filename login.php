07093d2f3e4d309aad5a1737ccdb504f89fd9735
VEnAcord <tokarevm2018@gmail.com>
2025-11-30 08:44:31 -1200

    Update login.php

--- login.php
+++ login.php
@@ -1,1 +1,20 @@
+<?php
+require_once "db.php";
+
+$username = $_POST["username"] ?? "";
+$password = $_POST["password"] ?? "";
+
+// НАВМИСНО ВРАЗЛИВИЙ SQL
+$sql = "SELECT * FROM users WHERE username='$username' AND password='$password'";
+
+echo "<h1>Executed SQL:</h1>";
+echo "<pre>$sql</pre>";
+
+$result = $pdo->query($sql)->fetch();
+
+if ($result) {
+    echo "<h2 style='color:green;'>Login SUCCESS</h2>";
+} else {
+    echo "<h2 style='color:red;'>Login FAILED</h2>";
+}
 
