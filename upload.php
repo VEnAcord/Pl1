04f29c051fb8d0d06db7db1dc3279e052ee17d49
VEnAcord <tokarevm2018@gmail.com>
2025-11-30 08:44:45 -1200

    Update upload.php

--- upload.php
+++ upload.php
@@ -1,1 +1,9 @@
+<?php
+$target = "uploads/" . basename($_FILES["file"]["name"]);
+
+// Небезпечне завантаження: дозволяє будь-які файли (php теж!)
+move_uploaded_file($_FILES["file"]["tmp_name"], $target);
+
+echo "<h1>File uploaded!</h1>";
+echo "<p>Path: <a href='$target'>$target</a></p>";
 
